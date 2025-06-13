ffd1019844a01ba52a6a1c242a7e078de7cb6ddb
int2itive <70378581+int2itive@users.noreply.github.com>
2025-06-13 10:57:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const stickySections = [...document.querySelectorAll('.sticky')];
+//const stickySections = [...document.querySelectorAll('.sticky')];
 let menuBtn = document.querySelector('.hamburger-menu');
+let audioBtn = document.querySelector('media-audio--container button');
 
 let images = [ 'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/pdl-prof-img-005__W45RwHr_qe20pfhxZ.png', 'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/pdl-prof-img-md2__iYgSM_M6gDha3Da.jpg',   'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/KP-IMG/pdl-prof-img-119-39M__yVb5suzX_SfaghTBKG.jpg', 'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/kpa--gal-IMG-070__RfAFDWNkV.png'
 ];
@@ -7,20 +8,20 @@ let images = [ 'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/pdl-prof
 let othrImages = [ 'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/scs-scrl-clp-07_gkMNyA3WQ.jpg', 'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/chin-scroll-clp-01_KfhofaCSd.jpg',   'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/jni-scrl-profile-15-002c_19nKnG7Yr.png', 'https://ik.imagekit.io/ghow2otb3rc/Projects/SoM/Artists/319722734_135851459309360_5051777608849275082_n_SvNz4xV4h5.jpg'
 ];
 
-images.forEach(img => {
-    stickySections.forEach(section => {
-        let image = document.createElement('img');
-        image.src = img;
-        section.querySelector('.scroll_section').appendChild(image);
-    });
-});
+//images.forEach(img => {
+//    stickySections.forEach(section => {
+//        let image = document.createElement('img');
+//        image.src = img;
+//        section.querySelector('.scroll_section').appendChild(image);
+//    });
+//});
 
-window.addEventListener('scroll', (e) => {
-   for(let i = 0; i < stickySections.length; i++) {
-       transform(stickySections[i]);
-   } 
+// window.addEventListener('scroll', (e) => {
+//    for(let i = 0; i < stickySections.length; i++) {
+//        transform(stickySections[i]);
+//    } 
    // progressIndicator();
-});
+// });
 
 function transform(section) {
     const offsetTop = section.parentElement.offsetTop;
@@ -32,6 +33,7 @@ function transform(section) {
 }
 
 let navigation = new TimelineLite({paused:true, reversed:true});
+
 navigation.to("#navigation--Wrap", 0.5, {opacity: 1, display: 'block'})
           .from(".menu", 0.5, {opacity: 0, y: 30})
           .from(".social", 0.5, {opacity: 0});
@@ -41,60 +43,30 @@ menuBtn.addEventListener('click', function() {
   document.querySelector('#navigation--Wrap').classList.toggle('showing');
 });
 
+audioBtn.addEventListener('click', function () {
+    let textScroller = this.querySelector('label-wrapper');
+    this.classList.add('is-on');
+    let audioOn = true;
+});
+
 
 /* Progress Indicator - JQuery */
 
 
-(function($) { "use strict";
+/* Progress Indicator - JQuery */
 
-	//Switch dark/light
-	
-	// $(".switch").on('click', function () {
-	// 	if ($("body").hasClass("light")) {
-	// 		$("body").removeClass("light");
-	// 		$(".switch").removeClass("switched");
-	// 	}
-	// 	else {
-	// 		$("body").addClass("light");
-	// 		$(".switch").addClass("switched");
-	// 	}
-	// });
-		
-	$(document).ready(function(){"use strict";
-	
-		//Scroll back to top
-		
-		var progressPath = document.querySelector('.progress-wrap path');
-		var pathLength = progressPath.getTotalLength();
-		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-		progressPath.style.strokeDashoffset = pathLength;
-		progressPath.getBoundingClientRect();
-		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
-		var updateProgress = function () {
-			var scroll = $(window).scrollTop();
-			var height = $(document).height() - $(window).height();
-			var progress = pathLength - (scroll * pathLength / height);
-			progressPath.style.strokeDashoffset = progress;
-		};
-		updateProgress();
-		$(window).scroll(updateProgress);	
-		var offset = 50;
-		var duration = 550;
-		jQuery(window).on('scroll', function() {
-			if (jQuery(this).scrollTop() > offset) {
-				jQuery('.progress-wrap').addClass('active-progress');
-			} else {
-				jQuery('.progress-wrap').removeClass('active-progress');
-			}
-		});				
-		jQuery('.progress-wrap').on('click', function(event) {
-			event.preventDefault();
-			jQuery('html, body').animate({scrollTop: 0}, duration);
-			return false;
-		});
-		
-		
-	});
-	
-})(jQuery);
+
+//(function($) { "use strict";
+
+  //Switch dark/light
+  
+  // $(".switch").on('click', function () {
+  //  if ($("body").hasClass("light")) {
+  //    $("body").removeClass("light");
+  //    $(".switch").removeClass("switched");
+  //  }
+  //  else {
+  //    $("body").addClass("light");
+  //    $(".switch").addClass("switched");
+  //  }
+  // });
